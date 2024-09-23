f27de19eaa73ec2138fd8b789435b3ecb138b56a
Isha Gupta <154554502+ishgupta127@users.noreply.github.com>
2024-09-23 11:57:49 +0100

    Add files via upload

---
+++ odefcnew (2).m
@@ -0,0 +1,30 @@
+function dydt = odefcnew(t,y,bctype,M,K,F,nel,tau,tamb,tboun)
+
+if bctype==1
+    
+    temp1=tamb+(tboun(1)-tamb)*exp(-t/tau);
+    temp2=tamb+(tboun(2)-tamb)*exp(-t/tau);
+    
+    dtemp1=(tboun(1)-tamb)*(-1/tau)*exp(-t/tau);
+    dtemp2=(tboun(2)-tamb)*(-1/tau)*exp(-t/tau);
+
+    
+    M(1,:)=[];
+    K(1,:)=[];
+    F(1,:)=[];
+
+    M(sum(nel),:)=[];
+    K(sum(nel),:)=[];
+    F(sum(nel),:)=[];
+
+F=-(M(:,1)*dtemp1)-(K(:,1)*temp1)-(M(:,sum(nel)+1)*dtemp2)-K(:,sum(nel)+1)*temp2; 
+
+M(:,1)=[];
+K(:,1)=[];
+M(:,sum(nel))=[];
+K(:,sum(nel))=[];
+
+end
+dydt=(M)\(F-K*y);
+
+end
